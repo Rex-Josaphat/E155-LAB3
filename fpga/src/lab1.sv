d2681298dc541c1bb06ae159d2dbc0e3b2ff338f
Josaphat UWIZEYE NGOGA <jngoga@g.hmc.edu>
2025-08-29 07:07:58 -0700

    Added 7 Segment Display control module

--- fpga/src/lab1.sv
+++ fpga/src/lab1.sv
@@ -57,5 +57,33 @@ endmodule
 
 
 ////////////////////////7 Segment LED Display Control////////////////////////
-// This Module controlls specific on-board in respose to the states of any of the 4 DIP switches.
-// 2 LEDs will light up following the switch states, while the 3rd one blinks at 2.4 Hz
+// This Module controlls the 7 segment display in response to the states of the 4 DIP switches
+// the interpreted hex value will be diplayed accurately using the segments of the diplay
+
+module Seven_SegDisp(
+        input logic reset,
+        input logic [3:0] swDIP,
+        output logic [6:0] SegDisp);
+
+        always_comb begin
+            case (swDIP)
+                4'h0 : SegDisp = 7'b1000000;
+                4'h1 : SegDisp = 7'b1111001;
+                4'h2 : SegDisp = 7'b0100100;
+                4'h3 : SegDisp = 7'b0110000;
+                4'h4 : SegDisp = 7'b0011001;
+                4'h5 : SegDisp = 7'b0010010;
+                4'h6 : SegDisp = 7'b0000010;
+                4'h7 : SegDisp = 7'b1111000;
+                4'h8 : SegDisp = 7'b0000000;
+                4'h9 : SegDisp = 7'b0011000;
+                4'hA : SegDisp = 7'b0001000;
+                4'hB : SegDisp = 7'b0000011;
+                4'hC : SegDisp = 7'b1000110;
+                4'hD : SegDisp = 7'b0100001;
+                4'hE : SegDisp = 7'b0000110;
+                4'hF : SegDisp = 7'b0001110;
+
+                default: SegDisp = 7'b1111111; // Default OFF
+            endcase            
+        end
