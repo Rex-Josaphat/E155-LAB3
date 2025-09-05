de12ed4be3fb265ef24bdbf8c1745066da61a5c1
Josaphat UWIZEYE NGOGA <jngoga@g.hmc.edu>
2025-09-05 09:34:29 -0700

    Added first iteration of the lab code

--- fpga/src/lab2_JN.sv
+++ fpga/src/lab2_JN.sv
@@ -2,3 +2,21 @@
 // jngoga@g.hmc.edu
 // 9/5/2025
 
+// This is the top module for this lab. It takes in reset and switch inputs 
+// and proceeds to instantiate the module responsible for perfoming the time-multiplexing
+// and controlling LEDs. It also handles 
+
+module top( 
+        input logic reset,
+        input logic [3:0] sw1, sw2,
+        output logic [1:0] onSeg,
+        output logic [4:0] segSum
+        output logic [6:0] segDisp);
+        
+        // Instantiate led control and time-multiplexing module
+        ledControl ledLogic(reset, sw1, sw2, segSum, sevenSegIn);
+
+        // Instantiate module to control 7-segment display
+        sevenSegDispCtrl segDispLogic(sevenSegIn, segDisp);
+
+endmodule
--- fpga/src/sevenSegDispCtrl.sv
+++ fpga/src/sevenSegDispCtrl.sv
@@ -2,33 +2,33 @@
 // jngoga@g.hmc.edu
 // 9/5/2025
 
-// This Module controlls the 7 segment display in response to the states of the 4 DIP switches.
+// This Module controlls the 7 segment display in response to the states of 4 DIP switches.
 // The interpreted hex value will be diplayed accurately using the segments of the diplay
 
 module sevenSegDispCtrl(
-        input logic [3:0] swDIP,
-        output logic [6:0] SegDisp);
+        input logic [3:0] sevenSegIn,
+        output logic [6:0] segDisp);
 
         always_comb begin
-            case (swDIP)
-                4'h0 : SegDisp = 7'b1000000;
-                4'h1 : SegDisp = 7'b1111001;
-                4'h2 : SegDisp = 7'b0100100;
-                4'h3 : SegDisp = 7'b0110000;
-                4'h4 : SegDisp = 7'b0011001;
-                4'h5 : SegDisp = 7'b0010010;
-                4'h6 : SegDisp = 7'b0000010;
-                4'h7 : SegDisp = 7'b1111000;
-                4'h8 : SegDisp = 7'b0000000;
-                4'h9 : SegDisp = 7'b0010000;
-                4'ha : SegDisp = 7'b0001000;
-                4'hb : SegDisp = 7'b0000011;
-                4'hc : SegDisp = 7'b1000110;
-                4'hd : SegDisp = 7'b0100001;
-                4'he : SegDisp = 7'b0000110;
-                4'hf : SegDisp = 7'b0001110;
+            case (sevenSegIn)
+                4'h0 : segDisp = 7'b1000000;
+                4'h1 : segDisp = 7'b1111001;
+                4'h2 : segDisp = 7'b0100100;
+                4'h3 : segDisp = 7'b0110000;
+                4'h4 : segDisp = 7'b0011001;
+                4'h5 : segDisp = 7'b0010010;
+                4'h6 : segDisp = 7'b0000010;
+                4'h7 : segDisp = 7'b1111000;
+                4'h8 : segDisp = 7'b0000000;
+                4'h9 : segDisp = 7'b0010000;
+                4'ha : segDisp = 7'b0001000;
+                4'hb : segDisp = 7'b0000011;
+                4'hc : segDisp = 7'b1000110;
+                4'hd : segDisp = 7'b0100001;
+                4'he : segDisp = 7'b0000110;
+                4'hf : segDisp = 7'b0001110;
 
-                default: SegDisp = 7'b1111111; // Default OFF
+                default: segDisp = 7'b1111111; // Default OFF
             endcase            
         end
 endmodule
