c150d97c7077e65c545ed96cb4e1f26b55ff9960
Josaphat UWIZEYE NGOGA <jngoga@g.hmc.edu>
2025-09-16 23:30:41 -0700

    Testbenches

--- fpga/sim/keypadScanner_tb.sv
+++ fpga/sim/keypadScanner_tb.sv
@@ -8,13 +8,13 @@ module keypadScanner_tb();
         logic clk; 
         logic reset;
         logic [3:0] col, col_async;
-        logic [3:0] row;
+        logic [3:0] row, rowScan;
         logic en;
 
         // instantiate device under test
 	    synchronizer dut1(clk, reset, col_async, col); // synchronizer module
 
-	    keypadScanner dut2(clk, reset, col, row, en);
+	    keypadScanner dut2(clk, reset, col, rowScan, row, en);
     
         // generate clock
 	    always begin
@@ -30,17 +30,35 @@ module keypadScanner_tb();
         end
 
 	    initial begin
-	    	col_async = 4'b0000; #10;
-	    	col_async = 4'b0001; #10;
+	    	col_async = 4'b0000; #50;
+	    	col_async = 4'b0001; #50;
 
 	    	col_async = 4'b0000; #10;
-	    	col_async = 4'b0010; #10;
+	    	col_async = 4'b0010; #50;
 
 	    	col_async = 4'b0000; #10;
-	    	col_async = 4'b0100; #10;
+	    	col_async = 4'b0100; #50;
 
 	    	col_async = 4'b0000; #10;
-	    	col_async = 4'b1000; #10;
+	    	col_async = 4'b1000; #50;
+
+			col_async = 4'b0000; #50;
+	    	col_async = 4'b1100; #50; // Bad input: Do not register
+
+			col_async = 4'b0000; #50;
+			col_async = 4'b1010; #50; // Bad input: Do not register
+
+			col_async = 4'b0000; #50;
+			col_async = 4'b1001; #50; // Bad input: Do not register
+
+			col_async = 4'b0100; #50; // Col Switch: No button release
+			col_async = 4'b1000; #50;
+
+			col_async = 4'b0011; #50; // Bad input: test release-hold (2 cols)
+			col_async = 4'b0001; #50;
+
+			col_async = 4'b1011; #50; // Bad input: test release-hold (3 cols)
+			col_async = 4'b1000; #50;
 	    end
 
 	
